004b5a5fcd8adfb856fa7a2c6f2d684de3606b34
Monica  Karasek <mlkarasek@gmail.com>
2018-08-09 04:44:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Monica"
+  end
+
+  get '/hometown' do
+    "My hometown is Portsmouth"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Under Pressure"
+  end
+
 end
